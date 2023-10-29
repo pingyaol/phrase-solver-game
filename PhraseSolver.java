4f080204c3a8cb357224f21cb55159325e384de0
pingyaol <110634925+pingyaol@users.noreply.github.com>
2023-10-29 16:51:06 -0700

    basic set up of variables and constructors

---
+++ PhraseSolver.java
@@ -0,0 +1,41 @@
+/*
+ * Activity 2.5.2
+ *
+ *  The PhraseSolver class the PhraseSolverGame
+ */
+import java.util.Scanner;
+  
+public class PhraseSolver
+{
+  /* your code here - attributes */
+  Player player1; 
+  Player player2; 
+  Board board; 
+  boolean solved; 
+
+  /* your code here - constructor(s) */ 
+  public PhraseSolver() {
+    player1 = new Player(); 
+    player2 = new Player(); 
+    board = new Board(); 
+    solved = false; 
+  }
+
+  public void play()
+  {
+    boolean solved = false;
+    int currentPlayer = 1;
+
+    Scanner input = new Scanner(System.in);
+    
+    boolean correct = true;
+    while (!solved) 
+    {
+      String guess = input.nextLine(); 
+      /* your code here - game logic */
+      solved = board.guessLetter(guess); 
+    } 
+   
+  }
+  
+}
